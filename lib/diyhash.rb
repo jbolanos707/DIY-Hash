201203ba22dc5e72ac857cbfcae1e131e0fda77c
Jessi Iler & Jennifer Bolanos <student@epicodus.com>
2015-04-27 12:01:56 -0700

    passed rspec for .store

--- lib/diyhash.rb
+++ lib/diyhash.rb
@@ -1,14 +1,17 @@
 class MyHash
 
   define_method(:initialize) do #initializing new class
+    @key = []
+    @value = []
   end
 
-  #define_method(:store) do |key, value|
-    #@key = key #setting instance variable"key"
-    #@value = value #setting instance variable "value"
-  #end
+  define_method(:store) do |key, value|
+    @key = key #setting instance variable"key"
+    @value = value #setting instance variable "value"
+  end
 
-  #define_method(:fetch) do
-    #@key.eq?(@value)
-  #end
+  define_method(:fetch) do
+    @value
+  end
+
 end
